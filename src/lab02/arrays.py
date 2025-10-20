b1e41cdbde7e447ea5000c37cad1e2abf5b97150
89234447300 <donskaya.lika07@mail.ru>
2025-10-20 13:48:07 +0300

    Create lab02

--- src/lab02/arrays.py
+++ src/lab02/arrays.py
@@ -14,6 +14,7 @@ test_cases = [
     [1.5, 2, 2.0, -3.1]
 ]
 
+print("Тестирование min_max")
 for i, test_data in enumerate(test_cases, 1):
     try:
         result = min_max(test_data)
@@ -22,3 +23,46 @@ for i, test_data in enumerate(test_cases, 1):
         print(f"{i}: {test_data} -> {e}")
 
 
+def unique_sorted(nums):
+
+    return sorted(set(nums))
+
+
+test_cases = [
+    [3, 1, 2, 1, 3],
+    [],
+    [-1, -1, 0, 2, 2],
+    [1.0, 1, 2.5, 2.5, 0]
+]
+
+print("Тестирование unique_sorted")
+for test in test_cases:
+    result = unique_sorted(test)
+    print(f"{test} -> {result}")
+
+
+def flatten(mat):
+    result = []
+
+    for item in mat:
+        if not isinstance(item, (list, tuple)):
+            raise TypeError(f"Элемент {item} не является списком или кортежем")
+
+        result.extend(item)
+
+    return result
+
+test_cases = [
+    [[1, 2], [3, 4]],
+    [[1, 2], (3, 4, 5)],
+    [[1], [], [2, 3]],
+    [[1, 2], "ab"] # вызывает TypeError
+]
+
+print("Тестирование flatten")
+for test in test_cases:
+    try:
+        result = flatten(test)
+        print(f"{test} -> {result}")
+    except TypeError as e:
+        print(f"{test} -> TypeError: {e}")
